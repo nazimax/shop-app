84c9df39a639e2333cd0f5a98696b6892fe2517d
nazimax <kissi.salim.yahia.@outlook.fr>
2018-06-24 21:06:31 +0100

    adding access control to avoid CORS errors

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,9 +9,14 @@ router.get('/',(req,res,next)=>{
 })
 
 router.post('/',(req,res,next)=>{
- 
+    const product={
+        name: req.body.name,
+        price :req.body.price
+    }
+
     res.status(201).json({
-        message:'handeling POST  requests to products'
+        message:'handeling POST  requests to products',
+        product:product
     })
 })
 
--- app.js
+++ app.js
@@ -2,9 +2,26 @@ const express =require('express')
 const app= express()
 const morgan =require('morgan')
 const productRoutes=require('./api/routes/products') 
+const bodyParser=require('body-parser')
+
 
 const orderRoutes =require('./api/routes/orders')
 app.use(morgan('dev'))
+
+app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.json())
+
+
+app.use((req,res,next)=>{
+
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, DELETE, PATCH, GET')
+        return res.status(200).json({});
+    }
+})
 
 app.use('/products',productRoutes)
 app.use('/orders',orderRoutes)
